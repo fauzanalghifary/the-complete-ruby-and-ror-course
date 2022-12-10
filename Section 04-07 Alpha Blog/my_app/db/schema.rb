191173540a6970b2f58314fb14472e57eb2956bb
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-12-10 15:40:59 +0700

    finish section 07

--- Section 04-07 Alpha Blog/my_app/db/schema.rb
+++ Section 04-07 Alpha Blog/my_app/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_160820) do
+ActiveRecord::Schema.define(version: 2022_12_10_055126) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +25,12 @@ ActiveRecord::Schema.define(version: 2022_12_08_160820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
