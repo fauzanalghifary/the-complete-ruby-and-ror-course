67aa03c7b4775f78b04ba20af4d572ce2ef1bfcd
fauzanalghifary <m.fauzanghifary@gmail.com>
2022-12-17 14:52:25 +0700

    finish section 08

---
+++ Section 08 MessageMe/my_app/app/controllers/chatrooms_controller.rb
@@ -0,0 +1,10 @@
+class ChatroomsController < ApplicationController
+
+    before_action :require_user
+
+    def index
+        @message = Message.new
+        @messages = Message.custom_display
+    end
+
+end
